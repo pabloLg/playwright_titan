a2376ff131b0ae9a14b035574adb7e085da20e76
pabloLg <plogioco@gmail.com>
2024-09-23 16:59:27 +0200

    url fix

--- playwright.config.js
+++ playwright.config.js
@@ -27,7 +27,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-     baseURL: 'https://qa01.titanos.tv',
+    baseURL: 'url here',
     headless: false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
